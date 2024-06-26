0b9e739aefe37f9f6ff7f2117fe5942e85431e33
Izadora <izadora.gomesmoura@gmail.com>
2024-06-26 08:55:27 -0300

    Controle de Gastos

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func main() {
+/*func main() {
 	despesas := []string{"mercado", "aluguel", "eletricidade"}
 
 	fmt.Println("Despesas a serem pagas:")
@@ -34,4 +33,29 @@ func main() {
 
 	total := len(despesas)
 	fmt.Printf("\nTotal de itens na lista de despesas: %d\n", total)
+}
+*/
+
+
+func main() {
+	// Definindo variáveis para os gastos fixos
+	var aluguel float64 = 1000.0
+	var internet float64 = 100.0
+	var transporte float64 = 300.0
+
+	// Calculando o total de gastos
+	totalGastos := aluguel + internet + transporte
+
+	// Imprimindo o total de gastos
+	fmt.Printf("Total de gastos mensais: R$ %.2f\n", totalGastos)
+
+	// Definindo o orçamento máximo permitido
+	orcamentoMaximo := 2000.0
+
+	// Verificando se o total de gastos excede o orçamento máximo
+	if totalGastos > orcamentoMaximo {
+		fmt.Println("Os gastos mensais excederam o orçamento máximo.")
+	} else {
+		fmt.Println("Os gastos mensais estão dentro do orçamento.")
+	}
 }
